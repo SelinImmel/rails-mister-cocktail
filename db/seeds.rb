31399de6ba90dd95187e78679da9505ae60a0baa
Selin Immel <selin.immel@whu.edu>
2019-05-23 22:36:42 +0800

    added some css

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,3 @@
-require 'open-uri'
-require 'json'
 
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
@@ -8,6 +6,8 @@ require 'json'
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'open-uri'
+require 'json'
 
 Ingredient.destroy_all
 url = "https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list"
